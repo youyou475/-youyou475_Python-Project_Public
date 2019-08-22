56c927db87547a5d21370865ba21b591e369706c
youyou475 <youyou475@hotmail.com>
2019-08-22 14:36:35 +0800

    20190822
    Follow the code specification.

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -1,23 +1,24 @@
+#-*-coding:utf-8-*-
 import random
 
 number = random.randint(1,10)
 guess1 = input("Guess This Number(From 1 to 10):")
 guess2 = int(guess1)
-guesstimes = 0
+guessTimes = 0
 
 if guess2 > number:
     print("It's smaller than yours.")
 if guess2 < number:
     print("It's bigger than yours.")
 
-while guesstimes < 3:
+while guessTimes < 3:
     guess1 = input("Try again:")
     guess2 = int(guess1)
     if guess2 > number:
         print("It's smaller than yours.")
     if guess2 < number:
         print("It's bigger than yours.")
-    guesstimes += 1
+    guessTimes = guessTimes + 1
     if guess2 == number:
         print("It's right!You are good!")
         break
